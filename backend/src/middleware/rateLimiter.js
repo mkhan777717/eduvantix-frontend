4556a2052570e9ebba27cf5c4c929e9e615599a4
mkhan777717 <mmdkhan007@gmail.com>
2026-07-10 16:34:17 +0530

    changed folder structure

--- backend/src/middleware/rateLimiter.js
+++
@@ -1,8 +0,0 @@
-// Pass-through middleware to disable rate limiting completely
-const noOpLimiter = (req, res, next) => next();
-
-module.exports = {
-  apiLimiter: noOpLimiter,
-  authLimiter: noOpLimiter,
-  submissionLimiter: noOpLimiter
-};
